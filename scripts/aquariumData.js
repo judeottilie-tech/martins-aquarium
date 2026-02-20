37aff439278cad88cfb92bd1f11dcdce44fce4ae
Jude Andersen <jude.ottilie@gmail.com>
2026-02-20 10:32:37 -0600

    update commit 02/20

--- scripts/aquariumData.js
+++ scripts/aquariumData.js
@@ -16,6 +16,118 @@ export const database = {
             diet: "Omnivore",
             image: "https://www.aquariumofpacific.org/images/made_new/images-olc-Palette_Sugeonfish_Blue_Tang_900_600_q85.jpg"
         },
+        {
+            name: "Bruce",
+            species: "Great White Shark",
+            length: 15,
+            location: "Indian Ocean",
+            diet: "Carnivore",
+            image: "https://upload.wikimedia.org/wikipedia/commons/5/56/White_shark.jpg"
+        },
+        {
+            name: "Pearl",
+            species: "Flapjack Octopus",
+            length: 5,
+            location: "Pacific Ocean",
+            diet: "Carnivore",
+            image: "https://featuredcreature.com/wp-content/uploads/2012/10/20090617093038942.jpg"
+        },
+        {
+            name: "Gill",
+            species: "Moorish Idol",
+            length: 9,
+            location: "Indo-Pacific",
+            diet: "Omnivore",
+            image: "https://greatbarrierreeftours.com/great-barrier-reef/great-barrier-reef-animals/moorish-idol/moorish-idol-9/"
+        },
+        {
+            name: "Bubbles",
+            species: "Yellow Tang",
+            length: 6,
+            location: "Hawaiian Waters",
+            diet: "Herbivore",
+            image: "https://www.waikikiaquarium.org/wp-content/uploads/2013/11/tang-yellow_620.jpg"
+        },
+        {
+            name: "Splash",
+            species: "Atlantic Salmon",
+            length: 30,
+            location: "North Atlantic Ocean",
+            diet: "Carnivore",
+            image: "https://static.inaturalist.org/photos/1020642/large.jpg"
+        },
+        {
+            name: "Coral",
+            species: "Lionfish",
+            length: 12,
+            location: "Indo-Pacific",
+            diet: "Carnivore",
+            image: "https://captainhooks.com/wp-content/uploads/2024/07/lionfish-florida-keys-invasive.webp"
+        },
+        {
+            name: "Flash",
+            species: "Mahi-Mahi",
+            length: 20,
+            location: "Tropical Atlantic",
+            diet: "Carnivore",
+            image: "https://pelagicgear.com/cdn/shop/articles/Mahi_Profile-HEADER-2.jpg?v=1608240224&width=1100"
+        },
+        {
+            name: "Ziggy",
+            species: "Zebrafish",
+            length: 3,
+            location: "Himalayan Region",
+            diet: "Omnivore",
+            image: "https://cdn.britannica.com/96/219196-050-9DD604F0/Zebra-Fish-Danio-Zebrafish.jpg"
+        },
+        {
+            name: "Marlin",
+            species: "Blue Marlin",
+            length: 45,
+            location: "Atlantic Ocean",
+            diet: "Carnivore",
+            image: "https://fishflags.com/cdn/shop/articles/blue.marlin.enio.morales_1100x.jpg?v=1589410327"
+        },
+        {
+            name: "Sunny",
+            species: "Sunfish",
+            length: 10,
+            location: "Temperate Oceans",
+            diet: "Carnivore",
+            image: "https://img1.wsimg.com/isteam/ip/c62a16d6-a784-41fb-b95b-6adff6e5c42c/IMG_3954.JPG/:/rs=w:1280"
+        },
+        {
+            name: "Ray",
+            species: "Manta Ray",
+            length: 18,
+            location: "Tropical Oceans",
+            diet: "Filter Feeder",
+            image: "https://images.squarespace-cdn.com/content/v1/5a196500914e6b09132e911f/1595922319847-AKO0JNUC35P8R9Z784IO/Oceanic+Manta+%283%29.jpg"
+        },
+        {
+            name: "Spike",
+            species: "Swordfish",
+            length: 15,
+            location: "Mediterranean Sea",
+            diet: "Carnivore",
+            image: "https://seahistory.org/wp-content/uploads/swordfish-2.jpg"
+        },
+        {
+            name: "Glimmer",
+            species: "Bioluminescent Lanternfish",
+            length: 6,
+            location: "Deep Ocean",
+            diet: "Carnivore",
+            image: "https://www.nwf.org/-/media/NEW-WEBSITE/Shared-Folder/Magazines/2018/Dec-Jan/Fish-Lantern-SolvinZanklNPL-425x292.jpg"
+        },
+        {
+            name: "Titan",
+            species: "Giant Grouper",
+            length: 21,
+            location: "Indo-Pacific Reefs",
+            diet: "Carnivore",
+            image: "https://upload.wikimedia.org/wikipedia/commons/3/3a/Epinephelus_quinquefasciatus2.jpg"
+        }
     ],
     tips: [
         {
--- scripts/fishList.js
+++ scripts/fishList.js
@@ -1,29 +1,70 @@
 import { database } from './aquariumData.js';
 
-export const fishList = () => {
-    // Generate an HTML representation of each fish
-    let fishHTML = '';
-    database.fish.forEach(fish => {
-        fishHTML += `
-            <div class="fish-card">
-                <h3>${fish.name}</h3>
-                <p>Species: ${fish.species}</p>
-                <p>Length: ${fish.length} inches</p>
-                <p>Location: ${fish.location}</p>
-                <p>Diet: ${fish.diet}</p>
-                <img src="${fish.image}" alt="${fish.name}">
-            </div>
-        `;
-    });
-    return fishHTML;
+export const mostHolyFish = () => {
+    let holyFish = "";
+
+    for (const fish of database.fish) {
+        if (fish.length % 3 === 0) {
+            holyFish += `
+                <div class="fish-card">
+                    <h3>${fish.name}</h3>
+                    <p>Species: ${fish.species}</p>
+                    <p>Length: ${fish.length} inches</p>
+                    <p>Location: ${fish.location}</p>
+                    <p>Diet: ${fish.diet}</p>
+                    <img src="${fish.image}" alt="${fish.name}">
+                </div>
+            `;
+        }
+    }
+
+    return holyFish;
 };
 
-export const renderFishToDOM = (fishHTML) => {
-   const fishContainer = document.getElementById('fish-list');
+export const soldierFish = () => {
+    let servants = "";
 
-   if (fishContainer) {
-       fishContainer.innerHTML = fishHTML;
-   } else {
-       console.error('Could not find element with id "fish-list"');
-   }
+    for (const fish of database.fish) {
+        if (fish.length % 5 === 0) {
+            servants += `
+                <div class="fish-card">
+                    <h3>${fish.name}</h3>
+                    <p>Species: ${fish.species}</p>
+                    <p>Length: ${fish.length} inches</p>
+                    <p>Location: ${fish.location}</p>
+                    <p>Diet: ${fish.diet}</p>
+                    <img src="${fish.image}" alt="${fish.name}">
+                </div>
+            `;
+        }
+    }
+
+    return servants;
+};
+
+export const regularFish = () => {
+    let unworthy = "";
+
+    for (const fish of database.fish) {
+        if (fish.length % 3 !== 0 && fish.length % 5 !== 0) {
+            unworthy += `
+                <div class="fish-card">
+                    <h3>${fish.name}</h3>
+                    <p>Species: ${fish.species}</p>
+                    <p>Length: ${fish.length} inches</p>
+                    <p>Location: ${fish.location}</p>
+                    <p>Diet: ${fish.diet}</p>
+                    <img src="${fish.image}" alt="${fish.name}">
+                </div>
+            `;
+        }
+    }
+
+    return unworthy;
+};
+
+export const renderFishToDOM = () => {
+    document.getElementById('holy-fish-list').innerHTML = mostHolyFish();
+    document.getElementById('soldier-fish-list').innerHTML = soldierFish();
+    document.getElementById('regular-fish-list').innerHTML = regularFish();
 };
